c82f462ce6498257b84e20f04b9c4976a9e81406
Ali Rezagholizadeh <alirezagholizadeh70@gmail.com>
2025-05-23 13:51:00 -0400

    ffmpeg added to between the sourec and the rtmp server. Problem exists in sensing the received stream by the server.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import asyncio
 import json
 import yaml
 import datetime
@@ -5,7 +6,7 @@ from scheduler.core import program_pipeline
 TIME_FORMAT = "%H:%M:%S"
 
 
-def main():
+async def main():
     # LOAD
     # load program schedule and config
     schedule_file = "schedule.json"
@@ -14,12 +15,12 @@ def main():
 
 
     # change some time variables to let us test the program now
-    schedule["program_schedule"]["start_time"] = (start:= datetime.datetime.now() + datetime.timedelta(seconds = 10)).time().strftime(format=TIME_FORMAT)
+    schedule["program_schedule"]["start_time"] = (start:= datetime.datetime.now() + datetime.timedelta(seconds = 10)).time()
     for ontime_indx in range(len((ontime_contents:= schedule["program_schedule"]["on-time_contents"]))):
         if(ontime_contents[ontime_indx]["name"] == "start_program"):
-            ontime_contents[ontime_indx]["play_time"]= start.time().strftime(format=TIME_FORMAT)
+            ontime_contents[ontime_indx]["play_time"]= start.time()
         else:
-            ontime_contents[ontime_indx]["play_time"] = (start + datetime.timedelta(seconds= 20)).time().strftime(format=TIME_FORMAT)
+            ontime_contents[ontime_indx]["play_time"] = (start + datetime.timedelta(seconds= 20)).time()
 
     print(f"{'-'*10}\nschedule: \n{schedule} \n{'-'*10}")
     # load config
@@ -30,16 +31,22 @@ def main():
     # access to the videos to play to build a streaming pipeline
     source_dir = config["program_source_path"]["video"]
     print(f"config type: {type(config)}")
+    program = program_pipeline(config=config, program_schedule=schedule)
+    server_task = asyncio.create_task(program.run_server())
+    schedule_task = asyncio.create_task(program.set_run_schedule())
 
-    program_pipeline(config = config, program_schedule = schedule)
+    print("main: awaiting schedule_task")
+    await schedule_task
+    print("main: awaiting server_task")
+    await server_task
     # each time a specific time-frame of the stream is loaded
     # start to play at a certain time with ability to find right time to play in the case of interruption.
 
     #
 
-    pass
+
 
 
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
--- scheduler/core.py
+++ scheduler/core.py
@@ -3,59 +3,63 @@ from typing import Dict
 import datetime
 import os
 import logging
-
-
+import ffmpeg
+from pathlib import Path
 from pyrtmp import StreamClosedException
 from pyrtmp.flv import FLVFileWriter, FLVMediaType
 from pyrtmp.session_manager import SessionManager
 from pyrtmp.rtmp import SimpleRTMPController, RTMPProtocol, SimpleRTMPServer
-from pyrtmp.rtmp import
+# from pyrtmp.rtmp import
 
 logging.basicConfig(level=logging.DEBUG)
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
 
 
-class RTMP2FLVController(SimpleRTMPController):
+class RTMPontroller(SimpleRTMPController):
 
-    def __init__(self, output_directory: str):
-        self.output_directory = output_directory
+    def __init__(self):
         super().__init__()
 
     async def on_ns_publish(self, session, message) -> None:
         publishing_name = message.publishing_name
-        file_path = os.path.join(self.output_directory, f"{publishing_name}.flv")
-        session.state = FLVFileWriter(output=file_path)
+        print(f"on publish. publishing_name: {publishing_name}")
         await super().on_ns_publish(session, message)
 
     async def on_metadata(self, session, message) -> None:
         session.state.write(0, message.to_raw_meta(), FLVMediaType.OBJECT)
+        print(f"on metadata: {message.to_raw_meta()}")
         await super().on_metadata(session, message)
 
     async def on_video_message(self, session, message) -> None:
         session.state.write(message.timestamp, message.payload, FLVMediaType.VIDEO)
+        print(f"on_video_message: {message.payload()}")
+
         await super().on_video_message(session, message)
 
     async def on_audio_message(self, session, message) -> None:
         session.state.write(message.timestamp, message.payload, FLVMediaType.AUDIO)
+        print(f"on_audio_message: {message.payload()}")
+
         await super().on_audio_message(session, message)
 
     async def on_stream_closed(self, session: SessionManager, exception: StreamClosedException) -> None:
         session.state.close()
+        print(f"on_stream_closed: {session.state}")
+
         await super().on_stream_closed(session, exception)
 
 
 
 class SimpleServer(SimpleRTMPServer):
 
-    def __init__(self, output_directory: str):
-        self.output_directory = output_directory
+    def __init__(self):
         super().__init__()
 
     async def create(self, host: str, port: int):
         loop = asyncio.get_event_loop()
         self.server = await loop.create_server(
-            lambda: RTMPProtocol(controller=RTMP2FLVController(self.output_directory)),
+            lambda: RTMPProtocol(controller=RTMPontroller()),
             host=host,
             port=port,
         )
@@ -80,18 +84,33 @@ class program_pipeline:
         self.program_schedule = program_schedule
         self.server_available_bool = False
         self.server_address = "127.0.0.1"
-        self.server_port = "2025"
+        self.server_port = "1935"
         self.server_stream_protocol = "rtmp"
 
-    def server(self):
+    async def run_server(self):
         """
         launch a RTMP serve to listen on coming stream and will emmit it over predefined options, like:
         - playback
 
         """
 
+        server = SimpleServer()
+        await server.create(host=self.server_address, port=self.server_port)
+        await server.start()
+        self.server_available_bool = True
 
-    async def trigger_timely_video(self, scheduled_time: datetime.time, video_dir:str):
+        await server.wait_closed()
+        print("server closed ..")
+
+    async def set_run_schedule(self):
+        time = self.program_schedule["program_schedule"]["start_time"]
+        assert isinstance(time, datetime.time), f"time is not in datetime.time format, it is: {type(time)}"
+        video_file_path = "/Users/earthisgreen/Documents/Programming Space/ُTo publish in Bitbucket or Git hub/GitHub/Non_Profit_Organizations/Noor_Cultural_Center/async-video-scheduler/source/video/body1/1-body_rain.mp4"
+        task = asyncio.create_task(self.trigger_timely_video(time, video_file_path))
+        print(f"trigger_timely_video called to be ran at {time} for {Path(video_file_path).parents[-3:]}")
+        await task
+
+    async def trigger_timely_video(self, scheduled_time: datetime.time, video_file_path:str):
         """
         Stands between the server and the local resources. It is set based on the scheduled time and video assigned.
         If time arrive, it will out the right timeslot of the video to the local RTMP server ran by the server method.
@@ -105,8 +124,20 @@ class program_pipeline:
             else:
                 if(datetime.datetime.now().time() >= scheduled_time):
                     #TODO: play the right timeslot of the video
-                    pass
+                    try:
+                        # out, err = ffmpeg.input(video_file_path).output(f"rtmp://{self.server_address}:{self.server_port}/test/sample", vcodec = "copy", acodec= "copy").run()
+                        process = ffmpeg.input(video_file_path, ss= "00:00:40").output(f"rtmp://{self.server_address}:{self.server_port}/", format='flv', vcodec='libx264').run_async(pipe_stdout=True, pipe_stderr=True)
+                         # "ffmpeg -i my_test_file.flv -c:v copy -c:a copy -f flv "
+                        print(f"trigger_timely_video: video ({video_file_path}) streamed to the server ..")
+                        process.wait()
+                        # print(f"trigger_timely_video: Error: {err},\n Out:{out}")
+                    except ffmpeg.Error as e:
+                        print(f"trigger_timely_video: An error occurred in loading video file to the server: {e}")
+                    
+                    break
                 else:
+                    print(f"now: {datetime.datetime.now().time()}, scheduled_time: {scheduled_time}")
+
                     await asyncio.sleep(0.5)
 
 
